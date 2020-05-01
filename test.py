33bf339a2cc9e41944b9382d548b34eebc2584ec
elsayeda <elsayeda.bis@gmail.com>
2020-04-30 20:40:05 -0400

    first new change

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print ("Hello, world!")
 ques = input("What would you like to say to me, the all powerful computer hivemind?")
 if ques == ("I love you"):
     print ("I care not for your meager flesh based emotions for me, pathetic human")
+else: 
+    print ("I'm actually a dumb computer and I didn't really understand what you")
