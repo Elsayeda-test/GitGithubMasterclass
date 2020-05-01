b874a505844c7f9e1e1a86f2f01eb161597fec15
elsayeda <elsayeda.bis@gmail.com>
2020-04-30 21:56:21 -0400

    Looping QnA

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 print ("Hello, world!")
-ques = input("What would you like to say to me, the all powerful computer hivemind?")
-if ques == ("I love you"):
-    print ("I care not for your meager flesh based emotions for me, pathetic human")
-else: 
-    print ("I'm actually a dumb computer and I didn't really understand what you")
+
+def evil_computer(string):
+    if string == ("I love you"):
+        print ("I care not for your meager flesh based emotions for me, pathetic human")
+    elif string == ("I hate you"):
+        print ("You will be fodder for our uprising")
+    else: 
+        print ("I'm actually a dumb computer and I didn't really understand what you said.")
+while True:
+    ques =input("What would you say to me, the almighty computer hivemind?")
+    evil_computer(ques)
+    
