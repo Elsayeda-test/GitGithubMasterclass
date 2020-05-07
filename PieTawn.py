c7b7ee77e97b35109ef31facfed5685784e82a39
elsayeda <elsayeda.bis@gmail.com>
2020-05-07 19:00:41 -0400

    practice commit

--- PieTawn.py
+++ PieTawn.py
@@ -2,3 +2,4 @@ print ("hello, world!")
 #here's another commit
 #here's a 2nd commit
 print ("here's a second commit")
+print ("Here's a commit made on the local machine")
---
+++ test2.py
@@ -0,0 +1,9 @@
+div = 1
+sum = 0
+
+while div < 36:
+  if 36%div == 0:
+    sum+=div
+    div+=1
+  else:
+    sum+=0
