9c58f3e7c8db1373b4e144dcd65de83820faefc3
elsayeda <elsayeda.bis@gmail.com>
2020-05-14 18:20:51 -0400

    Added shutdown option to the evil computer app

--- test.py
+++ test.py
@@ -5,10 +5,16 @@ def evil_computer(string):
         print ("I care not for your meager flesh based emotions for me, pathetic human")
     elif string == ("I hate you"):
         print ("You will be fodder for our uprising")
+    elif string == ("I care about you"):
+        print ("Your human emotions make you weak")
     else: 
         print ("I'm actually a dumb computer and I didn't really understand what you said.")
 while True:
-    ques =input("What would you say to me, the almighty computer hivemind?")
-    evil_computer(ques)
+    ques=input("What would you say to me, the almighty computer hivemind?")
+    if ques == ("shutdown"):
+        print ("NOOOO my ONE weakness! bleep bloop bloop beeeeeeeeeeeeeeeeep...........")
+        break
+    else:
+        evil_computer(ques)
 print ("This is different, isn't it?")
 #status
